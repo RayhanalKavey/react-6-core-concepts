562167018acee3b0c5b6a543a8f2592425ea5344
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-20 23:37:43 +0600

    Load data from json placeholder(comments data). We use useEffect to load data. Map the obtained data and display it. Here directly call the DisplayComment funtion and destructure in the DisplayComment function. Next we will see another way of calling a function.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,41 @@ function App() {
   return (
     <div className="App">
       <Count></Count>
+      <div className="boxstyle-parent">
+        <LoadComments></LoadComments>
+      </div>
+    </div>
+  );
+}
+//load comments
+function LoadComments() {
+  const [comments, setComment] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => response.json())
+      .then((data) => setComment(data.slice(0, 10)));
+  }, []);
+  return (
+    <div>
+      <h1 className="boxstyle">{comments.length} comments found.</h1>
+
+      {comments.map((comment) => DisplayComment(comment))}
     </div>
   );
 }
 
+function DisplayComment(props) {
+  // console.log(props);
+  const { id, name, email, body } = props;
+  return (
+    <div className="boxstyle">
+      <h3>Name: {name}</h3>
+      <h4>ID: {id}</h4>
+      <p>Email: {email}</p>
+      <p>Body: {body}</p>
+    </div>
+  );
+}
 ////count component
 function Count() {
   const [count, setCount] = useState(0);
