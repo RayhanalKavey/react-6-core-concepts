a801de9691f414682b4d1613393a565e802a9618
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-20 23:31:35 +0600

    Add count function where we can increase and decrease the number. We use useState function and add event handler in the button.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Count></Count>
     </div>
   );
 }
 
+////count component
+function Count() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  return (
+    <div
+      style={{
+        backgroundColor: "beige",
+        margin: "40px",
+        padding: "20px",
+        borderRadius: "10px",
+        border: "2px solid brown",
+      }}
+    >
+      <h1>Count:{count} </h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <br />
+      <br />
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  );
+}
 export default App;
